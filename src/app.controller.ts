069367622d687fb555002b8638c41552ddfd5774
黄炜杰 <huangweijie@hztianque.com>
2024-06-24 17:31:50 +0800

    路由守卫

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoginGuard } from './login.guard';
 
 // 主路径为 app
-@Controller("app")
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("app")
+  @UseGuards(LoginGuard)
   getHello(): object {
     console.log('get /app')
     return this.appService.getHello();
