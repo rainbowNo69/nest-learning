cf827461bc4c706484bee29599e607e3fa524e35
黄炜杰 <huangweijie@hztianque.com>
2024-07-02 16:45:11 +0800

    buffer学习

---
+++ test.js
@@ -0,0 +1,11 @@
+// const buf = Buffer.from('codewhy', 'ascii');
+// console.log(buf);
+// console.log(buf.toString());
+// console.log(buf.toString('ascii'));
+// const buf1 = Buffer.alloc(10);
+// console.log(buf1)
+
+// buf = Buffer.alloc(256);
+// len = buf.write("www.codewhy.net");
+// console.log("写入字节数 : "+  len);
+// console.log(buf.toString());
