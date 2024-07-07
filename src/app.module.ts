2787c83e44fcc09f57d719a2e87fcd2ef1fbd312
夜航星星亮 <1132842293@qq.com>
2024-07-07 21:11:18 +0800

    database module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,30 @@
-import { MiddlewareConsumer, Module, NestModule, OnApplicationBootstrap, OnModuleInit } from '@nestjs/common';
+import {MiddlewareConsumer,Module,NestModule,OnApplicationBootstrap,OnModuleInit,} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import envConfig from '../config/env';
 import { LogMiddleware } from './log.middleware';
-import { APP_GUARD } from '@nestjs/core';
-import { LoginGuard } from './login.guard';
+// import { APP_GUARD } from '@nestjs/core';
+// import { LoginGuard } from './login.guard';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService,
-  // {
-  //   provide:APP_GUARD,
-  //   useClass:LoginGuard
-  // }
+  providers: [
+    AppService,
+    // {
+    //   provide:APP_GUARD,
+    //   useClass:LoginGuard
+    // }
   ],
 })
-export class AppModule implements OnModuleInit,OnApplicationBootstrap,NestModule {
-  onModuleInit(){}
-  onApplicationBootstrap(){}
-  configure(consumer:MiddlewareConsumer){
-    consumer.apply(LogMiddleware).forRoutes('app*')
+export class AppModule
+  implements OnModuleInit, OnApplicationBootstrap, NestModule
+{
+  onModuleInit() {}
+  onApplicationBootstrap() {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes('app*');
   }
 }
